c991b6894dcbcd54ee05117cf9ce40cb4648f11a
Mateusz Stochel <mateuszstochel1995@gmail.com>
2021-02-20 17:46:19 +0100

    Desktop view and animation

--- src/app/features/projects/ProjectsPage.js
+++ src/app/features/projects/ProjectsPage.js
@@ -24,9 +24,13 @@ Sequelize, Ant Design`,
 ];
 
 const projects = projectsContent.map((project, i) => (
-  <div className="aboutpage__content">
+  <div className={`aboutpage__content aboutpage__content__animation-${i}`}>
     <h2 className={`aboutpage__content__title-${i}`}>{project.title}</h2>
-    <img src={project.img} alt={project.title} />
+    <img
+      className={`aboutpage__content__image-${i}`}
+      src={project.img}
+      alt={project.title}
+    />
     <div className="aboutpage__text-wrapper">
       <h3>{project.title}</h3>
       <p>{project.content}</p>
