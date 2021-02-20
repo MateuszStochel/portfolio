64618823af5bc420813627429bfa5bada89e6cb4
Mateusz Stochel <mateuszstochel1995@gmail.com>
2021-02-20 16:56:48 +0100

    SkillsPage mobileView

--- src/app/features/projects/ProjectsPage.js
+++ src/app/features/projects/ProjectsPage.js
@@ -21,25 +21,17 @@ project was created with React(hooks), Typescript, NodeJS,
 Sequelize, Ant Design`,
     img: img0,
   },
-  {
-    title: "shop",
-    content: `Search about upcoming artist's events and concerts in towns. You
-must be logged in to use all features available in projects. The
-project was created with React(hooks), Typescript, NodeJS,
-Sequelize, Ant Design`,
-    img: img0,
-  },
 ];
 
 const projects = projectsContent.map((project, i) => (
   <div className="aboutpage__content">
-    <h2>{project.title}</h2>
+    <h2 className={`aboutpage__content__title-${i}`}>{project.title}</h2>
     <img src={project.img} alt={project.title} />
     <div className="aboutpage__text-wrapper">
       <h3>{project.title}</h3>
       <p>{project.content}</p>
-      <button>Github</button>
-      <button>Live</button>
+      <button className={`aboutpage__content-button${i} button`}>Github</button>
+      <button className={`aboutpage__content-button${i} button`}>Live</button>
     </div>
   </div>
 ));
